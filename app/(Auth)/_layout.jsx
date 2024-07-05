acf1a968b4df1bdce3bcd3afcf62a79cf2d513e5
Purvansh Sahu <147030767+purvanshh@users.noreply.github.com>
2024-07-05 21:03:42 +0530

    Update _layout.jsx

--- app/(Auth)/_layout.jsx
+++ app/(Auth)/_layout.jsx
@@ -7,9 +7,9 @@ const AuthLayout = () => {
   return (
     <>
     <Stack>
-      <Stack.Screen name="Login" options={{headerShown: false}}
+      <Stack.Screen name="login" options={{headerShown: false}}
        />
-      <Stack.Screen name="Register" options={{headerShown: false}}
+      <Stack.Screen name="register" options={{headerShown: false}}
        />
     </Stack>
 
